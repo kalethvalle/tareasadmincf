13721fd5dd6043d7799b2f1d15412ece5cabc922
kalethvalle <vallekaleth@gmail.com>
2021-02-10 15:21:54 -0500

    modifica configuraciones static files

--- task/models.py
+++ task/models.py
@@ -5,7 +5,7 @@ class TaskManager(models.Model):
     taskTitle = models.CharField(max_length=50)
     taskDescrip = models.TextField()
     taskEnd = models.BooleanField(default=False)
-    taskFile = models.FileField(upload_to='staticfiles/%Y/%m/%d/', blank=True, null=True)
+    taskFile = models.FileField(upload_to='static/%Y/%m/%d/', blank=True, null=True)
     taskResol = models.BooleanField(default=False)
     taskFechaCrea = models.DateField()
 
